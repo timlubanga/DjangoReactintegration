187bfe2a2216176f7c1ce5a5d9dd6fe115687636
timlubanga <timlubanga@gmail.com>
2021-04-14 02:01:02 +0300

    configure babel webpack for frontend integration

---
+++ frontend/src/components/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <h1>welcome to react django integration</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById("app"));
